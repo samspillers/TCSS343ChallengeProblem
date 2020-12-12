3c07b9ca392b07ada8aad91acc455dae34fb5d55
supersam710 <spillers.sam@gmail.com>
2020-12-11 17:39:16 -0800

    Added timing

--- challenge.py
+++ challenge.py
@@ -1,8 +1,8 @@
+import asyncio
+import time
+
 from algorithm import min_squares, brute_force
 from board import Board
-
-if __name__ == '__main__':
-    print(brute_force(3, 3))
 
 def example():
     board1 = Board(8, 2)
@@ -33,3 +33,35 @@ def example():
     board3.add_queen((4,))
     board_array = board3.generate_board_array()
     print(board_array)
+
+async def async_function_test(the_function, output: list):
+    output.append(the_function())
+
+async def wait_15_minutes(output: list):
+    await asyncio.sleep(900)
+    output.append("Too long")
+
+if __name__ == '__main__':
+    print("Started...")
+    results = []
+    for i in range(3, 20):
+        for j in range(3, 20):
+            output = []
+            start_time = time.time()
+            asyncio.run(async_function_test(lambda: brute_force(i, j), output))
+            asyncio.run(wait_15_minutes(output))
+            while len(output) == 0:
+                time.sleep(10)
+            pending = asyncio.all_tasks()
+            for task in pending:
+                task.cancel()
+            if output[0] == "Too long":
+                print("n:", i, "d:", j, "Too long")
+                if j is 3:
+                    print("Results: " + str(results))
+                    quit()
+                else:
+                    break
+            else:
+                print("n:", i, "d:", j, "Time:", time.time() - start_time)
+                results.append((i, j, time.time() - start_time))
